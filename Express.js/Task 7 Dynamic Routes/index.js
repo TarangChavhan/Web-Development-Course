fe6658cbefe80a2cb719a5bc0a77cb6d647b7b97
TarangChavhan <chavhannaman2@gmail.com>
2026-05-22 18:06:16 +0530

    Dynamic Routes

---
+++ Express.js/Task 7 Dynamic Routes/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+const app = express();
+app.get("/", (req, resp) => {
+    const User = ['Naman', 'Tarang', 'Om', 'Suraj', 'Yash'];
+    let data = '<ul>';
+    for (let i = 0; i < User.length; i++) {
+        data += `
+               <li>
+               <a href="/User/${User[i]}">${User[i]}
+               </a>
+               </li>`;
+    }
+    resp.send(data)
+})
+
+app.get("/User/:name",(req,resp)=>{
+    console.log(req.params.name);
+    const display_name = req.params.name;
+    resp.send(`Hello i am ${display_name}`)    
+})
+
+app.listen(3000);
