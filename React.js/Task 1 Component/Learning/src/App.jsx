f1a94801416238c4543348618bf02b2c66f6254a
TarangChavhan <chavhannaman2@gmail.com>
2026-05-27 20:06:22 +0530

    Component

---
+++ React.js/Task 1 Component/Learning/src/App.jsx
@@ -0,0 +1,22 @@
+function App(){
+  return(
+    <div>
+      <First/>
+      <h1>Hello I am naman Chavhan</h1>
+      <Last></Last>
+    </div>
+  )
+}
+function First(){
+  return (
+  <div>
+    <h1>First Component</h1>
+  </div>
+  )
+}
+function Last(){
+  return (
+    "i am Last"
+  )
+} 
+export default App
