46f299bf2cbf5c43e81cc72bfe2af7494ad7509f
TarangChavhan <chavhannaman2@gmail.com>
2026-06-20 19:26:10 +0530

    Delete The Data From Database

--- React.js/To-Do List/Backend/index.js
+++ React.js/To-Do List/Backend/index.js
@@ -1,11 +1,26 @@
 import express from 'express'
 import { collectionName, connection } from './dbConfig.js';
 import cors from 'cors'
+import { ObjectId } from 'mongodb';
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
+
+
+app.delete("/delete/:id", async(req,resp)=>{
+    const db = await connection()
+    const collection = await db.collection(collectionName);
+    const result = await collection.deleteOne({_id:new ObjectId(req.params.id)});
+    if(result){
+        resp.send({message:"Task Deleted",sucess:true,result})
+    } else{
+        resp.send({message:"Task Not Deleted, Try again After Sometime...",sucess:false})
+    }
+    resp.send("Working...");
+})
+
 
 app.post("/add-task", async(req,resp)=>{
     const db = await connection()
--- React.js/To-Do List/Frontend/src/Components/Pages/list.jsx
+++ React.js/To-Do List/Frontend/src/Components/Pages/list.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import '../Style/list.css'
 const List = () => {
 
@@ -16,6 +16,16 @@ const List = () => {
       setTaskData(list.result)
     }
   }
+
+  const deleteTask = async (id) => {
+    let item = await fetch('http://localhost:3000/delete/'+id,{method:'delete'});
+    item = await item.json();
+    if (item.success) {
+      getListedData();
+      console.log("item Deleted Successfully");
+    }
+  }
+
   return (
     <div>
       <h2>Your Work List Is </h2>
@@ -27,11 +37,12 @@ const List = () => {
 
         {
           taskData && taskData.map((item, index) => (
-            <>
+            <Fragment key={item._id}>
               <li className='list-item'>{index + 1}</li>
               <li className='list-item'>{item.title}</li>
               <li className='list-item'>{item.Description}</li>
-            </>
+              <li className='list-item'><button onClick={()=>deleteTask(item._id)}>Delete</button> <button>Update</button></li>
+            </Fragment>
           ))
         }
       </ul>
